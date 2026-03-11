95c4787e270888650051f3846e5c34d722957f0e
Rabiya <rabiyaaiylchieva@gmail.com>
2026-03-11 15:40:58 +0600

    add BookSlider component with autoplay and coverflow effect

---
+++ src/shared/constants/index.ts
@@ -0,0 +1,48 @@
+import HarryPotterImage from '../assets/images/harry-potter.png';
+import RemarkImage from '../assets/images/remark.png';
+import BookHistoryImage from '../assets/images/book-history.png';
+
+export const SLIDER_BOOKS = [
+  {
+    id: 1,
+    image: HarryPotterImage,
+    title: 'Гарри Поттер и Тайная ко...',
+    author: 'Роулинг Джоан Кэтлин',
+    price: 450
+  },
+  {
+    id: 2,
+    image: RemarkImage,
+    title: 'Земля обетованная',
+    author: 'Эрих Мария Ремарк',
+    price: 350
+  },
+  {
+    id: 3,
+    image: BookHistoryImage,
+    title: 'История Книги',
+    author: 'Э. Эггер, А. Бахтияров',
+    price: 450
+  },
+  {
+    id: 4,
+    image: HarryPotterImage,
+    title: 'Гарри Поттер и Тайная ко...',
+    author: 'Роулинг Джоан Кэтлин',
+    price: 450
+  },
+  {
+    id: 5,
+    image: RemarkImage,
+    title: 'Земля обетованная',
+    author: 'Эрих Мария Ремарк',
+    price: 350
+  },
+  {
+    id: 6,
+    image: BookHistoryImage,
+    title: 'История Книги',
+    author: 'Э. Эггер, А. Бахтияров',
+    price: 450
+  }
+];
